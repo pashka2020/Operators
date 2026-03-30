5a3c003844a9404a90dc8e1d12bc13fb673bed6a
Neto test <pashtet201807@gmail.com>
2026-03-30 14:42:14 +0300

    dz

---
+++ Dz Data/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 20000;
+        int oneMilePrice = 20;
+
+        int miles = ticketPrice / oneMilePrice;
+
+        System.out.println("Начислено миль " + miles);
+    }
+}
